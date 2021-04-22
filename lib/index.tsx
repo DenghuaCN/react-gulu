aa2f123457619d89650f659b3ee890cd9b258e8e
denghua <denghua.fm@foxmail.com>
2021-04-22 20:23:45 +0800

    [+]添加配置文件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,8 @@
-console.log('hi')
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Button from './button'
+
+ReactDOM.render(
+    <Button></Button>,
+    document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
     entry: {
-        index: path.resolve(__dirname, './lib/index.tsx')
+        index: './lib/index.tsx'
     },
     output: {
         path: path.resolve(__dirname, './dist/lib'),
         library: 'DUI',
         libraryTarget: 'umd',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
     module: {
         rules: [{
