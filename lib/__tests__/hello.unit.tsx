4125c184f2e02dd27fbc93587386198d813d96af
denghua <denghua.fm@foxmail.com>
2021-05-10 23:22:17 +0800

    ceshi

--- lib/__tests__/hello.unit.tsx
+++ lib/__tests__/hello.unit.tsx
@@ -1,9 +1,7 @@
-function add(a:number, b: number) {
-    return a+b;
-}
+import sum from '../index';
 
 describe('我的第一个测试用例', ()=> {
     it('add(1,2) === 3', ()=> {
-        expect(add(1,2)).toEqual(3);
+        expect(sum(1,2)).toEqual(3);
     });
 });
